9b501d2b885bf2dd13eee1515f91155c91dd563a
thayour02 <you@example.com>
2026-08-15 16:45:35 +0100

    creat user and post route with database connection

---
+++ schema.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from pydantic import BaseModel,ConfigDict, Field,EmailStr
+
+
+
+
+class UserBase(BaseModel):
+    username: str = Field(min_length=1, max_length=50)
+    email: EmailStr = Field(min_length=1, max_length=120)
+
+class UserCreate(UserBase):
+    pass
+
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id:int
+    image_file: str | None
+    image_path: str
+
+
+
+
+class PostBase(BaseModel):
+    title: str=Field(min_length=1, max_length=100)
+    content:str=Field(min_length=1)
+
+
+class PostCreate(PostBase):
+    user_id: int
+
+
+class PostResponse(PostBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id:int
+    user_id: int
+    date_posted:datetime
+    author:UserResponse
+
+
